2f5d45cb96cf30388aa2f94832cb29a20350d19c
omk963 <132976939+omk963@users.noreply.github.com>
2024-05-04 20:20:43 -0700

    form and functionality added

--- j s/index.js
+++ j s/index.js
@@ -10,7 +10,7 @@ footer.appendChild(copyright);
 body.appendChild(footer);
 
 const skills = ["JavaScript", "HTML", "CSS", "GitHub", "React", "Python"];
-const skillsSection = document.querySelector("skills");
+const skillsSection = document.querySelector("#skills");
 const skillsList = skillsSection.querySelector('ul');
 
 for(let i = 0; i < skills.length; i++) {
@@ -18,3 +18,32 @@ for(let i = 0; i < skills.length; i++) {
     skill.innerHTML = skills[i];
     skillsList.appendChild(skill);
 }
+
+
+const messageForm = document.querySelector('form[name="leave_message"]');
+messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = event.target.usersName.value;
+    const email = event.target.usersEmail.value;
+    const message = event.target.usersMessage.value;
+
+    console.log(name, email, message);
+
+    const messageSection = document.querySelector("#messages");
+    const messageList = messageSection.querySelector('ul');
+    let newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href='mailto:${email}'>${name}</a>
+    <span>${message}</span>`;
+    let removeButton = document.createElement('button');
+    removeButton.innerHTML = 'remove';
+    removeButton.type = 'button';
+
+    removeButton.addEventListener('click', () => {
+        const entry = removeButton.parentNode;
+        entry.removeChild();
+    });
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    messageForm.reset();
+});
